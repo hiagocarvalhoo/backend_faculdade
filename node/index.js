c9170ce4e548d42878020222ee84874c14dad9c5
Hiago Carvalho <hiaagocarvalho@gmail.com>
2024-03-15 21:20:48 -0300

    Projeto de MongoDB

--- node/index.js
+++ node/index.js
@@ -4,21 +4,8 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
-const lista1 = require('./routes/lista1')
-
-app.use('/lista1', lista1)
-
-app.get('/', function(req, res){
-    res.send('Rota Principal')
-})
-
-app.get('/hello', function(req, res){
-    res.send('Chegou a resposta')
-})
-
-app.get('/nome', function(req, res){
-    res.send('Orion Teles de Mesquita')
-})
+const routes = require('./routes/routes')
+app.use('/', routes)
 
 app.listen(3000, function(){
     console.log('Server UP port 3000')
